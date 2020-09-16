adb1c22819bf0d2dcbdbfd3132cd19a685e959de
makkiboukhris <makki01boukhris@gmail.com>
2020-09-16 16:56:15 +0100

    Updates

---
+++ precache-manifest.c0e380a336760aadd31879ba50afa376.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02315062dee1908fb5212c241b07d19",
+    "url": "/jsx/index.html"
+  },
+  {
+    "revision": "7b98415d661d1420fdfd",
+    "url": "/jsx/static/css/main.d8df8c0a.chunk.css"
+  },
+  {
+    "revision": "2cc901bcc8434e628f3f",
+    "url": "/jsx/static/js/2.2eab61db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jsx/static/js/2.2eab61db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b98415d661d1420fdfd",
+    "url": "/jsx/static/js/main.593798ca.chunk.js"
+  },
+  {
+    "revision": "739a1e5c6c13149663da",
+    "url": "/jsx/static/js/runtime-main.28d00341.js"
+  },
+  {
+    "revision": "2eb3255f274f788a06ed957dbaa9ba6c",
+    "url": "/jsx/static/media/computer-programming-1.2eb3255f.jpg"
+  }
+]);
